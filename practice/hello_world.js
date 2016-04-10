91c24ad10442c2d07679612b6e039dcf193fc41c
shanazfc <schowdhery09@gmail.com>
2016-04-10 19:35:04 -0400

    file reorg

---
+++ practice/hello_world.js
@@ -0,0 +1,25 @@
+
+
+var speak = function() {
+  console.log('Hello, Johnny!')
+}
+
+
+speak();
+
+var hello = function() {
+  console.log("Hello, console.log")
+}
+
+hello();
+
+
+
+var person = {
+  name: 'Obama',
+  speak: function () {
+    console.log('Hello, World!')
+  }
+}
+
+person.speak();
