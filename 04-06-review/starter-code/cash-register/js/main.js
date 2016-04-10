2bbca5dcae3881d7b0859cedcee93756e13eca70
shanazfc <schowdhery09@gmail.com>
2016-04-10 19:33:16 -0400

    get the calculator decimal total to work

--- 04-06-review/starter-code/cash-register/js/main.js
+++ 04-06-review/starter-code/cash-register/js/main.js
@@ -1,50 +1,45 @@
 var $form = $('#entry');
 var $costInput = $('#newEntry');
+var total = 0;
 
 $(function() {
-	// when I submit the form, grab the input
+  // when I submit the form, grab the input
   $form.on('submit', function(event) {
     event.preventDefault();
     var entry = $costInput.val();
     var entry = parseFloat(entry);
     var currency = currencyFormat(entry);
-		//if the form is empty, NO!
+    //if the form is empty, NO!
     if (entry==0 || $.isNumeric(entry)==false ){
       alert("You shall not pass!");
     }
     //otherwise
-		else {
-		//pull the cost out every time I submit and replace with nothing.
-		$costInput.val("");
-		// take cost and add to the table
-		function appendNumber(entry){
-      //create the variables for <tr> and <td>
-			var $tr = $('<tr>');
-			var $td = $('<td class="item">');
-			//update the table
-			$td.html(currency);
-			$tr.append($td);
-			$("#entries").append($tr);
-    } //end appendNumber
-			//run the function
-			appendNumber(entry);
-      console.log("Johnny is awesome! Thank you! You rock^7no1NTgu1T^");
+    else {
+      //pull the cost out every time I submit and replace with nothing.
+      $costInput.val("");
+      // take cost and add to the table
+      function appendNumber(entry){
+        //create the variables for <tr> and <td>
+        var $tr = $('<tr>');
+        var $td = $('<td class="item">');
+        //update the table
+        $td.html(currency);
+        $tr.append($td);
+        $("#entries").append($tr);
+      } //end appendNumber
+      //run the function
+      appendNumber(entry);
       // get each of the entries
       var entries = $(".item").map(function() {
         return $(this).html().substring(1);
       }).get();
-      console.log(entries);
       //add the entries
-      var total = 0;
-      for (var i = 0; i < entries.length; i++) {
-        total += entries[i] << 0;
-      }
+      total += entry
       //print the total
-      $('#total').html("$" + total);
+      $('#total').html(currencyFormat(total));
     } //end else
   });//end form submit function
 });
-
 
 // turn entry into currency format
 function currencyFormat(number) {
