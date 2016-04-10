2157c90d5e54cfee6fd3f9952a43a8842bd9b292
shanazfc <schowdhery09@gmail.com>
2016-04-10 18:20:48 -0400

    class files upload part 4

---
+++ 03-28-objects-and-json/js-calculator_1/calculator.js
@@ -0,0 +1,24 @@
+var calculator = {
+  add: function(a,b){
+    return a + b;
+  },
+  subtract: function(a,b){
+    return a - b;
+  },
+
+  multiply: function(a,b){
+    return a * b;
+  },
+  divide: function(a,b){
+    return a / b;
+  },
+  exp: function(a,b){
+    for(var i = 1; i < b; i++);
+  }
+}
+
+console.log(calculator.add(1,2))
+console.log(calculator.subtract(1,2))
+console.log(calculator.multiply(1,2))
+console.log(calculator.divide(1,2))
+console.log(calculator.exp(2,3))
